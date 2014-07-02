d5294c13427eb838863a5a95bd8d4028752d81ea
Alexander Reiff <alexander.reiff@me.com>
2014-07-02 15:19:39 -0700

    Add options to limit computed font-size to original, ref width offset

--- jquery.scalem.js
+++ jquery.scalem.js
@@ -1,6 +1,6 @@
 /*!
 * Scalem v1.0.2 - A responsive text jQuery plugin
-* Copyright 2014, Tom Doan (http://www.tohodo.com/)
+* Copyright 2014, Tom Doan (http://www.tohodo.com/), Alexander Reiff (http://alexndr.me)
 *
 * Scalem by Tom Doan is licensed under the MIT License.
 * Read a copy of the license in the LICENSE file or at
@@ -10,13 +10,16 @@
 (function($) {
   $.fn.scalem = function(oOptions) {
     var oSettings = $.extend({
-        ratio: .5,        /* Scale ratio (1 = 100%) */
-        reference: null,  /* Text will scale relative to this element */
-        styles: ''        /* List of styles to scale (useful for buttons) */
+        ratio: .5,                  /* Scale ratio (1 = 100%) */
+        reference: null,            /* Text will scale relative to this element */
+        styles: ''                  /* List of styles to scale (useful for buttons) */,
+        referenceWidthOffset: 0     /* Sets the width to subtract from reference element's width for calculating new CSS values */,
+        doNotExceedOriginal: false  /* Limits the maximum value of the font size to the original value */
       }, oOptions),
       updateStyles = function(o, e) {
         var $o = $(o),
           /* Create clone to get true text width */
+          origFontSize = parseInt($o.css('font-size').replace('px','')),
           $o2 = $o.clone().css({
             'width': 'auto',
             'display': 'none',
@@ -29,7 +32,7 @@
           /* Scale ratio */
           nRatio = parseFloat(o.getAttribute('data-scale-ratio')) || oSettings.ratio,
           /* Reference width (set to parent width by default) */
-          nRefWidth = ($ref.length > 0) ? $ref.width() : $o.parent().width(),
+          nRefWidth = (($ref.length > 0) ? $ref.width() : $o.parent().width()) - oSettings.referenceWidthOffset,
           nTargetWidth = nRefWidth * nRatio,
           /* Text width */
           nTextWidth;
@@ -41,16 +44,22 @@
           $o2.remove();
           return;
         }
+        var newFontSize = 0;
         // Scale the text! (6px is minimum font size to get accurate ratio)
         for (var i=Math.round((6/$o2.css('font-size', '6px').width())*nTargetWidth), o2=$o2[0]; i<nTargetWidth; i++) {
           // Update font-size using native method for better performance
           // (see http://jsperf.com/style-vs-csstext-vs-setattribute)
           o2.style.fontSize = i + 'px';
           if ($o2.width() / nRefWidth > nRatio) {
-            $o.css('font-size', (i - 1) + 'px');
+            newFontSize = i - 1;
             break;
           }
         }
+        console.log(newFontSize, origFontSize);
+        if (oSettings.doNotExceedOriginal) {
+          newFontSize = Math.min(newFontSize, origFontSize);
+        }
+        $o.css('font-size', newFontSize + 'px');
         // Clean up
         $o2.remove();
         // Scale additional styles
